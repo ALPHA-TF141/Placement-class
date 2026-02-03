85e85b9af4b8a3ef036aaaf25419308806c3f0d9
ALPHA-TF141 <lmariaimmanuel@gmail.com>
2026-02-03 20:15:43 +0530

    Complete Week 1 Applied Programming Skills tasks

---
+++ Full Stack Application Development/lecture-Tasks/class2.js
@@ -0,0 +1,105 @@
+/*
+//using console.log
+function hi()
+{
+    console.log("Helloworld")
+}9
+hi()
+
+//add using function parameter
+function add(){
+    console.log(a+b);
+}
+var a=1;
+var b=20;
+add(a,b);
+
+/*
+//function call
+var factor="Kamal";
+var fplayer="Dhoni";
+var fmovie="Anbe Sivam";
+function call(){
+    console.log("Favourite Actor =",factor);
+    console.log("Favourite Player =",fplayer);
+    console.log("Favourite Movie =",fmovie);
+}
+call();
+
+
+//get value and print result
+function area(l,b){
+    console.log(l*b);
+}
+area(10,50);
+
+
+//return type:it is used within a function to specify the values that the function should produce as its result or return 
+function area(l,b){
+    return l*b;
+}
+var a=area(1,2);
+console.log(a);
+
+
+//if else concept 1
+function area(l,b){
+    if(l>b)
+    {
+        return l*b;
+    }
+    else {
+        return false;
+    }
+
+}
+var a=area(1,2);
+console.log(a);
+
+//if else concept 2
+a=10
+console.log(a)
+rain=false
+if(rain){
+    console.log("Take an Umbrella")
+}
+else{
+    console.log("Enjoy the sunshine")
+}
+
+
+//if else exercise
+color1="Red";
+
+
+if(color1 =="Red"){
+    console.log("stop")
+}
+if(color1 =="yellow"){
+    console.log("Get ready")
+}
+if(color1 =="green"){
+    console.log("Go")
+}
+
+
+//if else exercise 2
+score=30
+if(score > 50 & score < 70){
+    console.log("good job!");
+}
+if(score > 70){
+    console.log("Excellent performance!")
+}
+if(score < 50){
+    console.log("you need to improve")
+}
+*/
+//for loop -> initialization, condition, execution
+
+/*DOM -> Document Object Model :
+    an interface for web document that represents their structure as a tree-like object.
+    It allows dynamic manipulation of page content using languages like Javascript. 
+  DOM Manipulation:
+  process of prgrammatically    */
+
